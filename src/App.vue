974e4908555e814ae1e25fc180e4a03b33fbeac1
semyin <semyin@kplocker.com>
2021-12-18 14:25:02 +0800

    feat: 环境变量

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <script setup lang="ts">
+console.log(import.meta.env.VITE_APP_ENV)
 </script>
 
 <template>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
